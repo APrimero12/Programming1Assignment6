32b461499eafd7b17a222298002c37806689fdc7
%APrimero12% <laprimero2004@gmail.com>
2024-05-24 22:18:02 -0400

    "finished adding isPostalCodeValid method to the Address class"

--- src/main/java/org/example/Address.java
+++ src/main/java/org/example/Address.java
@@ -1,5 +1,14 @@
 package org.example;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+@Getter
+@Setter
 public class Address {
 
     private int streetNo;
@@ -9,4 +18,59 @@ public class Address {
     private String postalCode;
     private String country;
 
+    public Address(int streetNo, String street, String city, String province, String postalCode, String country) {
+        if (isPostalCodeValid(postalCode)) {
+            this.streetNo = streetNo;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.postalCode = postalCode.toUpperCase();
+            this.country = country;
+        } else {
+            this.streetNo = 0;
+            this.street = null;
+            this.city = null;
+            this.province = null;
+            this.postalCode = null;
+            this.country = null;
+        }
+    }
+
+    public static boolean isPostalCodeValid(String postalCode) {
+    if (postalCode == null) {
+        return false;
+    }
+
+    postalCode = postalCode.toUpperCase();
+
+    if (postalCode.length() == 6) {
+        return validateSixCharPostalCode(postalCode);
+    } else if (postalCode.length() == 7) {
+        return validateSevenCharPostalCode(postalCode);
+    } else {
+        return false;
+    }
+}
+
+    private static boolean validateSixCharPostalCode(String postalCode) {
+        for (int i = 0; i < postalCode.length(); i++) {
+            if (i % 2 == 0) {
+                if (Character.isLetter(postalCode.charAt(i))) {
+                    return false;
+                }
+            } else {
+                if (Character.isDigit(postalCode.charAt(i))) {
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean validateSevenCharPostalCode(String postalCode) {
+        if (postalCode.charAt(3) != ' ') {
+            return false;
+        }
+        return validateSixCharPostalCode(postalCode.replace(" ", ""));
+    }
 }
