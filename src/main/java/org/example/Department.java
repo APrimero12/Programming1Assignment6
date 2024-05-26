a606f3a41291f55b1078fad439b2e9cc480d57fd
%APrimero12% <laprimero2004@gmail.com>
2024-05-25 20:36:16 -0400

    "finished the Department class"

---
+++ src/main/java/org/example/Department.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import lombok.*;
+
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Getter
+public class Department {
+
+    private String departmentId;
+    private String departmentName;
+
+    private static int nextId = 1;
+
+
+
+
+    public Department(@NonNull String departmentId, String departmentName) {
+        if (validateDepartmentName(departmentName)) {
+            this.departmentId = null;
+            this.departmentName = null;
+        } else {
+            this.departmentId = "" + nextId++;
+            this.departmentName = departmentName;
+
+        }
+    }
+
+    public static boolean validateDepartmentName(String departmentName) {
+        if (departmentName == null) {
+            return false;
+        }
+        return true;
+    }
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+}
