e3cfc3312d9ca54e577762838956741e49c12405
%APrimero12% <laprimero2004@gmail.com>
2024-05-25 20:44:40 -0400

    "Created a Student class"

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Student {
+
+    private String studentId;
+    private String studentName;
+    private Gender gender;
+    private Address Address;
+    private Department department;
+    private ArrayList<Course> registeredCourses;
+
+    private static int nextId = 1;
+
+    public static boolean registerCourse(Course course) {
+
+    }
+}
